4c242f64424df7cff2f32f6bc90eaec102de4e19
Alexey Valikov <aleksei.valikov@gmail.com>
2018-06-23 08:31:01 +0200

    WIP #6.

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 'use strict'
 
+const textOf = (el) => {
+	if (!el || !el.children) return null
+	const c = el.children.find(c => c.type === 'text')
+	return c && c.value || null
+}
+
+
 const parse = (_, transformCoords = noTransform, stride = 2) => {
 	// todo
 	return null
 }
+
+const parseCoords = (s, ctx, opts) => {
+	ctx = ctx || {};
+	const srsDimension = ctx.srsDimension || 2;
+	// TODO AV: Check srsDimension?
+
+	const coords = s.replace(/\s+/g, ' ').trim().split(' ');
+	if (coords.length === 0 || (coords.length % srsDimension) !== 0) {
+		throw new Error(`Invalid coordinates list, number of coordinates ${coords.length} must be divisible by the srsDimension ${srsDimension}.`);
+	}
+
+	const points = [];
+	for (let i = 0; i < (coords.length - 1); i += srsDimension) {
+		const point = coords.slice(i, i + srsDimension).map(parseFloat)
+		points.push(...point);
+	}
+	return points;
+}
+
+
+Object.assign(parse, {
+	parseCoords
+})
 
 module.exports = parse
--- test.js
+++ test.js
@@ -1,14 +1,42 @@
 'use strict'
 
-const test = require('tape')
-const h = require('hyper-xml')
-const rewind = require('@turf/rewind')
+const test = require('tape');
+const h = require('hyper-xml');
+const rewind = require('@turf/rewind');
 
-const parse = require('.')
+const parse = require('.');
 
-const transformCoords = (...vals) => vals.map(val => val * 10)
+test('parseCoords > default', (t) => {
+	t.deepEqual(parse.parseCoords(' 140. -35 '), [140, -35]);
+	t.end();
+});
 
-test('todo', (t) => {
-	// todo
-	t.end()
-})
+test('parseCoords > 2D', (t) => {
+	t.deepEqual(parse.parseCoords(' 140. -35 ', { srsDimension: 2 }), [140, -35]);
+	t.end();
+});
+
+test('parseCoords > 2D with srsDimension 3', (t) => {
+	t.throws(() => parse.parseCoords('1 2', { srsDimension: 3 }), /Invalid coordinates list, number of coordinates 2 must be divisible by the srsDimension 3./);
+	t.end();
+});
+
+
+test('parseCoords > 3D', (t) => {
+	t.deepEqual(parse.parseCoords('140. -35 17.5', { srsDimension: 3 }), [140, -35, 17.5]);
+	t.end();
+});
+
+test('parseCoords > 3D with srsDimension 2 by default', (t) => {
+	t.throws(() => parse.parseCoords('1 2 3'), /Invalid coordinates list, number of coordinates 3 must be divisible by the srsDimension 2./);
+	t.end();
+});
+
+test('parseCoords > 3D with srsDimension 2 explicitly', (t) => {
+	t.throws(() => parse.parseCoords('1 2 3', { srsDimension: 2 }), /Invalid coordinates list, number of coordinates 3 must be divisible by the srsDimension 2./);
+	t.end();
+});
+
+
+
+
